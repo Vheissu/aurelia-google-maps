50f271097c05abab0ccfb848d5fa32f82e001bd8
Dwayne Charrington <dwaynecharrington@gmail.com>
2015-09-10 10:30:55 +1000

    Fixing up a typo and adding in ability to load Google API script (if not already loaded).

--- src/google-maps.js
+++ src/google-maps.js
@@ -7,16 +7,20 @@ export class GoogleMaps {
     constructor(element, ea) {
         this.element = element;
         this.ea = ea;
+
+        this.apikey = '';
     }
 
     attached() {
         var classRef = this;
 
+        console.log('Attached');
+
         this.element.addEventListener('dragstart', evt => {
             evt.preventDefault();
         });
 
-        this.ea.subscrube('google.maps.ready', () => {
+        this.ea.subscribe('google.maps.ready', () => {
             this.map = new google.maps.Map(this.element);
             this.map.markers = {};
             this.map.shapes = {};
@@ -25,6 +29,8 @@ export class GoogleMaps {
         window.myGoogleMapsCallback = function() {
             classRef.ea.publish('google.maps.ready');
         };
+
+        this.loadApiScript();
     }
 
     getCurrentPosition() {
@@ -40,7 +46,7 @@ export class GoogleMaps {
     loadApiScript() {
         if (window.google === undefined || window.google.maps === undefined) {
             let scriptEl = document.createElement('script');
-            scriptEl.src = 'https://maps.googleapis.com/maps/api/js&callback=myGoogleMapsCallback';
+            scriptEl.src = `https://maps.googleapis.com/maps/api/js?key=${this.apikey}&callback=myGoogleMapsCallback`;
             document.body.appendChild(scriptEl);
         }
     }
