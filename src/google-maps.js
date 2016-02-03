3879541ee8aca717f5e19235b4ee2ba03d792bd1
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-02-03 23:52:25 +1000

    fix(element): add in bindables for ID and tweak logic

--- src/google-maps.js
+++ src/google-maps.js
@@ -4,6 +4,10 @@ import {EventAggregator} from 'aurelia-event-aggregator';
 @customElement('google-map')
 @inject(Element, EventAggregator)
 export class GoogleMaps {
+    @bindable id = null;
+    @bindable markers = {};
+    @bindable shapes = {};
+
     constructor(element, ea) {
         this.element = element;
         this.ea = ea;
@@ -14,14 +18,16 @@ export class GoogleMaps {
     attached() {
         var classRef = this;
 
-        console.log('Attached');
+        if (this.id == null) {
+            this.id = guid();
+        }
 
         this.element.addEventListener('dragstart', evt => {
             evt.preventDefault();
         });
 
         this.ea.subscribe('google.maps.ready', () => {
-            this.map = new google.maps.Map(this.element);
+            this.map = new google.maps.Map(document.getElementById(this.id));
             this.map.markers = {};
             this.map.shapes = {};
         });
@@ -54,4 +60,14 @@ export class GoogleMaps {
     error() {
         console.log.apply(console, arguments);
     }
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
