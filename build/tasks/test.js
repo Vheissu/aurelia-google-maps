bce366bbc3c94aba62f7f6bfb1fddfeb1d79afc6
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-03-08 10:41:25 +1000

    chore(all): updating build process and paths for dependencies to later versions

--- build/tasks/test.js
+++ build/tasks/test.js
@@ -1,46 +1,34 @@
 var gulp = require('gulp');
-var karma = require('karma').server;
+var karma = require('karma');
+var coveralls = require('gulp-coveralls');
 
 /**
  * Run test once and exit
  */
 gulp.task('test', function (done) {
-    karma.start({
+    new karma.Server({
         configFile: __dirname + '/../../karma.conf.js',
         singleRun: true
     }, function(e) {
-        done();
-    });
+        done(e === 0 ? null : 'karma exited with status ' + e);
+    }).start();
 });
 
 /**
  * Watch for file changes and re-run tests on each change
  */
 gulp.task('tdd', function (done) {
-    karma.start({
+    new karma.Server({
         configFile: __dirname + '/../../karma.conf.js'
     }, function(e) {
         done();
-    });
+    }).start();
 });
 
 /**
- * Run test once with code coverage and exit
+ * Report coverage to coveralls
  */
-gulp.task('cover', function (done) {
-  karma.start({
-    configFile: __dirname + '/../../karma.conf.js',
-    singleRun: true,
-    reporters: ['coverage'],
-    preprocessors: {
-      'test/**/*.js': ['babel'],
-      'src/**/*.js': ['babel', 'coverage']
-    },
-    coverageReporter: {
-      type: 'html',
-      dir: 'build/reports/coverage'
-    }
-  }, function (e) {
-    done();
-  });
+gulp.task('coveralls', ['test'], function (done) {
+  gulp.src('build/reports/coverage/lcov/report-lcovonly.txt')
+    .pipe(coveralls());
 });
