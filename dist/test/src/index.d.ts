35d37e63226c50d9d08ea4bd35bbedd956f41d6b
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-01-17 23:16:02 +1000

    chore(plugin): remove test folder from dist

--- dist/test/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
-import { Configure } from './configure';
-import { GoogleMaps } from './google-maps';
-export declare function configure(aurelia: FrameworkConfiguration, configCallback?: (config: Configure) => Promise<any>): void;
-export { Configure };
-export { GoogleMaps };
--- dist/test/test/main.d.ts
+++
@@ -1,7 +0,0 @@
-declare let allTestFiles: string[];
-declare let TEST_REGEXP: RegExp;
-interface Window {
-    __karma__: any;
-    require: any;
-}
-declare let started: boolean;
