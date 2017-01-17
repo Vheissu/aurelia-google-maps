8a7786627df0f18de176cad3793b86ecba6d14e0
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-01-18 09:20:16 +1000

    test(google-map): simplifying test for google map (not tested)

--- test/unit/google-maps.ts
+++ test/unit/google-maps.ts
@@ -1,40 +1,29 @@
-import {TaskQueue} from 'aurelia-task-queue';
-import {BindingEngine} from 'aurelia-binding';
-import {EventAggregator} from 'aurelia-event-aggregator';
-
-import {Configure} from '../../src/configure';
-
-import { GoogleMaps } from '../../src/google-maps';
-
-let mockElement = document.createElement('div');
+import {StageComponent} from 'aurelia-testing';
+import {bootstrap} from 'aurelia-bootstrapper';
 
 describe('google maps', () => {
-    let sut: any;
-    let mockTaskQueue = new TaskQueue();
-    let mockConfigure = new Configure();
-    let mockBindingEngine = new BindingEngine();
-    let mockEventAggregator = new EventAggregator();
+    let component: any;
 
     beforeEach(() => {
-        sut = new GoogleMaps(mockElement, mockTaskQueue, mockConfigure, mockBindingEngine, mockEventAggregator);
+        component = StageComponent
+            .withResources('src/google-map')
+            .inView('<google-map latitude="37.323" longitude="-122.0527"></google-map>');
     });
 
     it('test passes', () => {
         expect(true);
     });
 
-    it('defaults are set', () => {
-        expect(sut.element).toEqual(mockElement);
-        expect(sut.taskQueue).toEqual(mockTaskQueue);
-        expect(sut.config).toEqual(mockConfigure);
-        expect(sut.bindingEngine).toEqual(mockBindingEngine);
-        expect(sut.eventAggregator).toEqual(mockEventAggregator);
+    it('should render map', done => {
+        component.create(bootstrap).then(() => {
+            const mapElement = document.querySelector('src/google-map');
+            expect(mapElement.innerHTML).not.toBe('');
+            done();
+       }).catch(e => console.log(e.toString()));
     });
-
-    it('expect loadApiScript to have been called', () => {
-        spyOn(sut, 'loadApiScript').and.callThrough();
-        sut.constructor(mockElement, mockTaskQueue, mockConfigure, mockBindingEngine, mockEventAggregator);
-        expect(sut.loadApiScript).toHaveBeenCalled();
+    
+    afterEach(() => {
+        component.dispose();
     });
 
 })
